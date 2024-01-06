22c339b4ad7a05fe501ea6ff2020a546b34db12e
Maxon laptop <vasmoto00@gmail.com>
2024-01-06 15:46:45 +0100

    ocaml skiponacci

--- lab6/OCaml/Untitled.ml
+++ lab6/OCaml/Untitled.ml
@@ -1,54 +1,21 @@
-type 'a lmbt = unit -> 'a lnode
-and 'a lnode =
-  | LLeaf
-  | LNode of 'a * 'a lmbt Lazy.t * 'a lmbt Lazy.t
+let rec fib n =
+  if n <= 2 then 1
+  else fib (n - 1) + fib (n - 2)
 
-type direction = Left | Right
+let rec skipponacciHelper n m =
+  match (n, m) with
+  | (1, 1) -> 1
+  | (_, 1) -> fib n
+  | (_, _) -> skipponacciHelper (2 * n - 1) (m - 1) + skipponacciHelper (2 * n) (m - 1)
 
-let memoize f =
-  let result = ref None in
-  fun () ->
-    match !result with
-    | Some v -> v
-    | None ->
-      let v = f () in
-      result := Some v;
-      v
+let rec skipD n m acc =
+  if n != 0 then
+    skipD (n - 1) m ((skipponacciHelper n m) :: acc)
+  else
+    acc
 
-let rec memoized_lazy_tree : int lmbt =
-  memoize (fun () ->
-    LNode (
-      1,
-      lazy (fun () ->
-        LNode (
-          2,
-          lazy (fun () -> LNode (4, lazy (fun () -> LLeaf), lazy (fun () -> LLeaf))),
-          lazy (fun () -> LLeaf)
-        )
-      ),
-      lazy (fun () ->
-        LNode (
-          3,
-          lazy (fun () -> LNode (5, lazy (fun () -> LLeaf), lazy (fun () -> LLeaf))),
-          lazy (fun () -> LNode (7, lazy (fun () -> LLeaf), lazy (fun () -> LLeaf)))
-        )
-      )
-    )
-  )
+let skipponacciDeclaracci n m =
+  List.rev (skipD n m [])
 
-let rec lttake (lazy_tree : 'a lmbt) (path : direction list) : 'a list =
-  let rec traverse tree path =
-    match tree () with
-    | LLeaf -> []
-    | LNode (value, left, right) ->
-      match path with
-      | [] -> [value]
-      | Left :: rest -> value :: traverse (Lazy.force left) rest
-      | Right :: rest -> value :: traverse (Lazy.force right) rest
-  in
-  traverse lazy_tree path
-;;
-
-(* Przykładowe użycie *)
-
-let result = lttake memoized_lazy_tree [Right; Left; Right]
+(* Testy *)
+let result1 = skipponacciDeclaracci 3 3;;
